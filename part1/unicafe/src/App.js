349c03722fbd11fc58c55728c2e046cd4e706e55
sianzhan <sianzhan@gmail.com>
2023-03-15 23:12:54 +0800

    feat(part1/unicafe): step4, change to show stats if there is feedback
    Change the application to display statistics only once feedback has been gathered.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,6 +11,10 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / all
   const positive = good / all * 100
 
+  if (all === 0) {
+    return <div>No feedback given</div>
+  }
+  
   return (
     <>
       <Display name='good' value={good} />
