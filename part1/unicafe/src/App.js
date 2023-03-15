474a928bbaccc0a20adea97db19912dd3bb86f69
sianzhan <sianzhan@gmail.com>
2023-03-15 23:04:23 +0800

    refactor(part1/unicafe): step3, extract statistics display as component
    Refactor the application so that displaying the statistics is extracted into its own Statistics component.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,15 +6,28 @@ const Button = props => <button onClick={props.handleClick}>{props.text}</button
 
 const Display = props => <div>{props.name} {props.value}</div>
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
+
+  return (
+    <>
+      <Display name='good' value={good} />
+      <Display name='neutral' value={neutral} />
+      <Display name='bad' value={bad} />
+      <Display name='all' value={all} />
+      <Display name='average' value={average} />
+      <Display name='positive' value={positive + ' %'} />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = good / all * 100
 
   return (
     <div>
@@ -24,12 +37,7 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
 
       <Header text='statistics' />
-      <Display name='good' value={good} />
-      <Display name='neutral' value={neutral} />
-      <Display name='bad' value={bad} />
-      <Display name='all' value={all} />
-      <Display name='average' value={average} />
-      <Display name='positive' value={positive + ' %'} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
