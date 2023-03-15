3375e978c5539467720763d0ed782630406fd135
sianzhan <sianzhan@gmail.com>
2023-03-15 21:17:03 +0800

    refactor(part1/courseinfo): refactor App component
    Refactor App body into three new components: Header, Content, and Total.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,9 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => props.parts.map((part) => <p key={part.name}>{part.name} {part.exercises}</p>)
+
+const Total = (props) => <p>Number of exercises {props.exercises.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}</p>
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +15,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={[{ name: part1, exercises: exercises1 }, { name: part2, exercises: exercises2 }, { name: part3, exercises: exercises3 }]} />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
 }
