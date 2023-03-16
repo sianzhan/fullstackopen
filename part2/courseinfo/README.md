9bcfb12b786af6b6ee26e1fea8a081f8a084944e
sianzhan <sianzhan@gmail.com>
2023-03-16 15:32:08 +0800

    chore(part2/courseinfo): clone courseinfo from part1 to part2

---
+++ part2/courseinfo/README.md
@@ -0,0 +1,15 @@
+# Exercises 1.1.-1.5. : Course Information
+
+This is a simple React application that displays information about a course with its name, parts, and the total number of exercises.
+
+## Usage
+
+To run the application, clone the repository and run the following command in your terminal:
+
+### `npm start`
+
+Which runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,45 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => <p>Number of exercises {props.parts.reduce((accumulator, currentPart) => accumulator + currentPart.exercises, 0)}</p>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
