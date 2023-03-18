e873467fdb8c31caaf2b9b19b9f68b12ca65d815
sianzhan <sianzhan@gmail.com>
2023-03-18 18:07:57 +0800

    feat(part2/phonebook): step4, add search field for filtering by name
    Implement a search field that can be used to filter the list of people by name (case-insensitive).

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,20 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
 
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
 
     const isDuplicate = persons.some(person => person.name === newName)
@@ -38,16 +40,31 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const searchTermLowerCase = searchTerm.toLowerCase()
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={searchTerm} onChange={handleSearchTermChange} /></div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div><button type="submit">add</button></div>
       </form>
+
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
+
+      {
+        persons
+          .filter(person => person.name.toLowerCase().includes(searchTermLowerCase))
+          .map(person => <div key={person.id}>{person.name} {person.number}</div>)
+      }
     </div>
   )
 }
