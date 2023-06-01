bbc0be17d99091d2173fac7283551a9cda4d2891
sianzhan <sianzhan@gmail.com>
2023-06-01 09:21:02 +0800

    feat(part3/phonebook): step3, add /persons/:id for single phonebook entry

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (request, response) => {
   const info = `<p>Phonebook has info for ${persons.length} people</p>`
   const date = `<p>${new Date()}</p>`
