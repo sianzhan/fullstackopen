878de68e5794588218bc4d60dc9de17e68439046
sianzhan <sianzhan@gmail.com>
2023-06-02 10:53:49 +0800

    feat(part3/phonebook-database): step1-2, change so that entries are fecthed from/posted to mongodb

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 var morgan = require('morgan')
-const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
+
+const app = express()
 
 morgan.token('post-data', function (req, res) { return (req.method == 'POST') ? JSON.stringify(req.body) : '' })
 
@@ -10,50 +14,29 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 app.use(express.json())
 app.use(express.static('build'))
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({})
+    .then(persons => {
+      if (persons) {
+        response.json(persons)
+      } else {
+        response.status(404).end()
+      }
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+  const id = request.params.id
 
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
 })
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -63,7 +46,6 @@ app.post('/api/persons', (request, response) => {
   if (!body) error = 'body is missing'
   else if (!body.name) error = "name can't be empty"
   else if (!body.number) error = "number can't be empty"
-  else if (persons.find(person => person.name === body.name)) error = "name must be unique"
 
   if (error) {
     return response.status(400).json({
@@ -71,15 +53,24 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save()
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => {
+      if (error.code === 11000) {
+        response.status(400).json({
+          error: "name must be unique"
+        })
+      } else {
+        console.error('An unexpected error occurred:', error);
+      }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
